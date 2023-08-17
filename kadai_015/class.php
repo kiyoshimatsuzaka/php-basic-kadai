617a24764dd8614290a82548f4fe3a4473ff6996
kiyoshimatsuzaka <121368695+kiyoshimatsuzaka@users.noreply.github.com>
2023-08-17 16:43:16 +0900

    Add files via upload

--- kadai_015/class.php
+++ kadai_015/class.php
@@ -14,12 +14,16 @@
              // プロパティを定義する                        
              private $name;
              private $price;
-             public $c_price;
  
              // コンストラクタを定義する
              public function __construct(string $name, int $price) {
                  $this->name = $name;
                  $this->price = $price;
+             }
+             //メソッドを追加（今回追加）
+            function show_price() {
+                $show_price = '25';
+                echo $show_price . '<br>';
              }
          }
  
@@ -43,7 +47,13 @@
                   $this->height = $height;
                   $this->weight = $weight;
                   }
-              }
+              //メソッドを追加(今回追加)
+              function show_height() {
+                $show_height = '60';
+                echo $show_height . '<br>';
+               }
+
+          }
         
           // インスタンス化する
             $Animal = new Animal('dog', 60 ,5000);
@@ -51,8 +61,9 @@
           // インスタンス$userの各プロパティの値を出力する
             print_r($Animal);
             echo '<br>';
-
-
+          //メソッドを実行（今回追加）
+          $Food->show_price();
+          $Animal->show_height();
          ?>
      </p>
 </body>
