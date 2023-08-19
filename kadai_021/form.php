5e4547fff727f80f2e3a05a5622c9c98c1f391fb
kiyoshimatsuzaka <121368695+kiyoshimatsuzaka@users.noreply.github.com>
2023-08-19 16:07:54 +0900

    Add files via upload

--- kadai_021/form.php
+++ kadai_021/form.php
@@ -17,13 +17,13 @@
                 </td>
             </tr>
             <tr>
-            <tr>
                 <td>性別</td>
                 <td>
                     <input type="radio" name="user_gender" value="男性" checked>男性
                     <input type="radio" name="user_gender" value="女性">女性
                 </td>
             </tr>
+            <tr>
                 <td>メールアドレス</td>
                 <td>
                     <input type="text" name="user_email">
